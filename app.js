4b4e73ee7891096cc96dc902140fee497bb74734
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2025-02-01 23:29:55 -0800

    Add route and data testing for contact form

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ import colors from "colors";
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3030;
 
+let contactTest = [];
+
 app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post("/submit-info", (req, res) => {
+  contactTest.push(req.body);
+  res.send(
+    `<h1>Thank you for submitting ${req.body.fName}. I will respond soon</h1>`
+  );
+});
+
+app.get("/admin/contacts", (req, res) => {
+  res.send(contactTest);
 });
 
 app.listen(PORT, () => {
