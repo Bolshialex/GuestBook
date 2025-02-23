fecfda034d3753bb7d6b6a080b42f2842f68042d
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2025-02-22 16:04:01 -0800

    Fin Guestbook part III

--- app.js
+++ app.js
@@ -5,22 +5,51 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 const PORT = 3030;
 
-let contactTest = [];
+let contacts = [];
 
 app.get("/", (req, res) => {
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
+  res.render("home");
 });
 
 app.post("/submit-info", (req, res) => {
-  contactTest.push(req.body);
-  res.sendFile(`${import.meta.dirname}/views/success.html`);
+  const contact = {
+    fName: req.body.fName,
+    lName: req.body.lName,
+    jobTitle: req.body["job-title"],
+    company: req.body.company,
+    linkedIn: req.body["linked-in"],
+    email: req.body.email,
+    pastMeeting: req.body["past-meeting"],
+    otherMeet: req.body["other-meet"],
+    message: req.body.message,
+    mailingList: req.body["mailing-list"],
+    emailFormat: req.body["email-format"],
+    timestamp: new Date().toLocaleString(),
+  };
+
+  if (contact.fName.trim() === "") {
+    res.send("<h1>Invalid First Name</h1>");
+    return;
+  }
+  if (contact.lName.trim() === "") {
+    res.send("<h1>Invalid Last Name</h1>");
+    return;
+  }
+  if (contact.email.trim() === "") {
+    res.send("<h1>Invalid Email</h1>");
+    return;
+  }
+
+  contacts.push(contact);
+  res.render("success", { contact });
 });
 
 app.get("/admin/contacts", (req, res) => {
-  res.send(contactTest);
+  res.render("admin", { contacts });
 });
 
 app.listen(PORT, () => {
